2e299b9d86127df3040c2ded9eaf5ac11139baac
simnilss <sim.f.nilsson@gmail.com>
2017-03-19 13:16:13 +0100

    Added support for separate host/device code

--- include/sini/CudaCompat.h
+++ include/sini/CudaCompat.h
@@ -6,8 +6,14 @@
 
 #ifdef __CUDACC__
 #define SINI_CUDA_COMPAT inline __host__ __device__
+#define SINI_CUDA_KERNEL __global__
 #else
 #define SINI_CUDA_COMPAT inline
+#endif
+
+// Allows for device/host specific CUDA code
+#if defined(__CUDACC__) && defined(__CUDA_ARCH__)
+#define SINI_CUDA_DEVICE
 #endif
 
 #endif
