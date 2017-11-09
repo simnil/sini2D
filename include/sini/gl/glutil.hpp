01e25bc6919fff518688895c822768d65c5659c4
Simon Nilsson <sim.f.nilsson@gmail.com>
2017-11-09 21:42:01 +0100

    loadShaderProgram now handles geometry shaders and attribute pointer binding via a given function pointer, if provided

--- include/sini/gl/glutil.hpp
+++ include/sini/gl/glutil.hpp
@@ -11,13 +11,21 @@ namespace sini { namespace gl {
 	// Load and compile a single shader
 	GLuint loadShader(const char* shader_src, uint32_t shader_type);
 
-	// Load and compile a vertex and fragment shader
-	// The shader program is not linked, as attribute locations need to be bound
-	// beforehand. Linking is done with linkShaderProgram().
-	GLuint loadShaderProgram(const char* vertex_shader_src, const char* fragment_shader_src) noexcept;
-
-	// Link a shader program previously loaded with loadShaderProgram()
+	// Link a previously loaded shader program
 	bool linkShaderProgram(GLuint shader_program) noexcept;
+
+	// Compile and load a vertex and fragment shader
+	// If attribute locations aren't explicitly bound in the shader source code
+	// a pointer to a function binding them can be provided
+	GLuint loadShaderProgram(const char* vertex_shader_src, const char* fragment_shader_src,
+		void(*attrib_bind_func_ptr)(uint32_t shader_program) = nullptr) noexcept;
+
+	// Compile and load a vertex, geometry and fragment shader
+	// If attribute locations aren't explicitly bound in the shader source code
+	// a pointer to a function binding them can be provided
+	GLuint loadShaderProgram(const char* vertex_shader_src, const char* geomentry_shader_src,
+		const char* fragment_shader_src,
+		void(*attrib_bind_func_ptr)(uint32_t shader_program) = nullptr) noexcept;
 
 }} // namespace gl, namespace sini
 
--- src/sini/gl/glutil.cpp
+++ src/sini/gl/glutil.cpp
@@ -17,14 +17,14 @@ namespace sini { namespace gl {
 		return shader;
 	}
 
-	GLuint loadShaderProgram(const char* vertex_shader_src, const char* fragment_shader_src) noexcept {
+	GLuint loadShaderProgram(const char* vertex_shader_src,	const char* fragment_shader_src,
+		void(*attrib_bind_func_ptr)(uint32_t shader_program)) noexcept {
 
 		GLuint vertex_shader = loadShader(vertex_shader_src, GL_VERTEX_SHADER);
 		if (vertex_shader == 0) {
 			std::cerr << "Vertex shader could not be compiled" << std::endl;
 			return 0;
 		}
-
 		GLuint fragment_shader = loadShader(fragment_shader_src, GL_FRAGMENT_SHADER);
 		if (fragment_shader == 0) {
 			std::cerr << "Fragment shader could not be compiled" << std::endl;
@@ -35,9 +35,21 @@ namespace sini { namespace gl {
 		glAttachShader(shader_program, vertex_shader);
 		glAttachShader(shader_program, fragment_shader);
 
+		if (attrib_bind_func_ptr)
+			attrib_bind_func_ptr(shader_program);
+
+		bool link_success = linkShaderProgram(shader_program);
+
+		glDetachShader(shader_program, vertex_shader);
+		glDetachShader(shader_program, fragment_shader);
 		glDeleteShader(vertex_shader);
 		glDeleteShader(fragment_shader);
 
+		if (!link_success) {
+			std::cerr << "Could not link shader program" << std::endl;
+			glDeleteShader(shader_program);
+			return 0;
+		}
 		return shader_program;
 	}
 
@@ -48,6 +60,58 @@ namespace sini { namespace gl {
 		GLint link_ok;
 		glGetProgramiv(shader_program, GL_LINK_STATUS, &link_ok);
 		return static_cast<bool>(link_ok);
+	}
+
+	GLuint loadShaderProgram(const char* vertex_shader_src, const char* geometry_shader_src,
+		const char* fragment_shader_src,
+		void(*attrib_bind_func_ptr)(uint32_t shader_program)) noexcept {
+
+		GLuint vertex_shader = loadShader(vertex_shader_src, GL_VERTEX_SHADER);
+		if (vertex_shader == 0) {
+			std::cerr << "Vertex shader could not be compiled" << std::endl;
+			return 0;
+		}
+		GLuint geometry_shader;
+		bool skip_geom_shader = (geometry_shader_src == nullptr
+			|| geometry_shader == NULL);
+		if (!skip_geom_shader) {
+			geometry_shader = loadShader(geometry_shader_src, GL_GEOMETRY_SHADER);
+			if (geometry_shader == 0) {
+				std::cerr << "Geometry shader could not be compiled" << std::endl;
+				return 0;
+			}
+		}
+		GLuint fragment_shader = loadShader(fragment_shader_src, GL_FRAGMENT_SHADER);
+		if (fragment_shader == 0) {
+			std::cerr << "Fragment shader could not be compiled" << std::endl;
+			return 0;
+		}
+
+		GLuint shader_program = glCreateProgram();
+		glAttachShader(shader_program, vertex_shader);
+		if (!skip_geom_shader) glAttachShader(shader_program, geometry_shader);
+		glAttachShader(shader_program, fragment_shader);
+
+		if (attrib_bind_func_ptr)
+			attrib_bind_func_ptr(shader_program);
+
+		bool link_success = linkShaderProgram(shader_program);
+
+		glDetachShader(shader_program, vertex_shader);
+		glDetachShader(shader_program, fragment_shader);
+		glDeleteShader(vertex_shader);
+		glDeleteShader(fragment_shader);
+		if (!skip_geom_shader) {
+			glDetachShader(shader_program, geometry_shader);
+			glDeleteShader(geometry_shader);
+		}
+
+		if (!link_success) {
+			std::cerr << "Could not link shader program" << std::endl;
+			glDeleteShader(shader_program);
+			return 0;
+		}
+		return shader_program;
 	}
 
 }} // namespace gl, namespace sini
