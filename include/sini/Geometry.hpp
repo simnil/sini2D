df46f0670241b91835c2bf115a596e237d2b0e74
Simon Nilsson <sim.f.nilsson@gmail.com>
2017-12-09 01:42:22 +0100

    Added header to include all geometry files in the library

---
+++ include/sini/Geometry.hpp
@@ -0,0 +1,9 @@
+// Common header to include all basic geometry headers in sini2D
+
+#pragma once
+#ifndef SINI_GEOMETRY_H
+#define SINI_GEOMETRY_H
+
+#include "sini/geometry/Line.hpp"
+
+#endif // SINI_GEOMETRY_H
